bb521e4415df613616d53f05c20d368d8ca07b06
GHjas06 <124841085+GHjas06@users.noreply.github.com>
2023-02-08 15:06:16 -0800

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title ('My parents New Healthy Diner')
-streamit.header('Breakfast menu')
+streamlit.header('Breakfast menu')
 streamlit.text('Omega & Blueberry Smoothie')
 streamlit.text('Kale, spinach & rocky smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
